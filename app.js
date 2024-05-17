4e443a15aee7347255c5e74178e64fdebec6fdc6
mathan4 <mathansmart5862@gmail.com>
2024-05-17 22:55:47 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT
+
+app.get("/api/v1",(request,response)=>{
+    response.status(200).send({message:"Server Running"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running at http://localhost:${PORT}`)
+})
